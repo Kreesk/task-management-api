68e6539c94d7d085ae322dce070a2cff8d579171
Kreesk <dwql67@gmail.com>
2025-03-12 19:42:39 +0300

    Add basic Task API (GET, POST)

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ import asyncio
 import uvicorn
 
 from asgiref.wsgi import WsgiToAsgi
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 asgi_app = WsgiToAsgi(app)
+api = Api(app)
+
+tasks = []
+
+class TaskResource(Resource):
+    def get(self):
+        return {'task': tasks}
+
+    def post(self):
+        data = request.get_json()
+        new_task = {'id' : len(tasks) + 1, 'title' : data['title'], 'status' : 'todo'}
+        tasks.append(new_task)
+        return new_task, 201
+
+api.add_resource(TaskResource, '/tasks')
 
 @app.route('/')
 def index():
