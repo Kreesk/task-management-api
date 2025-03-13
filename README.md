3bdb98e366f83c767958ba6b71ec410469a38f86
Kreesk <dwql67@gmail.com>
2025-03-13 20:36:34 +0300

    Initial commit: Task Management API

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# task-management-api
-Task Management System with REST API and web interface using Flask
+# Task Management API
+
+Простое веб-приложение для управления задачами, построенное на Flask и Flask-RESTful. Поддерживает добавление задач и их просмотр через REST API и HTML-интерфейс.
+
+## Возможности
+- Добавление новых задач через POST-запросы к `/api/tasks`.
+- Просмотр списка задач через GET-запросы к `/api/tasks` или HTML-страницу `/tasks`.
+- Асинхронный запуск с использованием `uvicorn`.
+
+## Установка
+1. Склонируйте репозиторий:
+   git clone https://github.com/Kreesk/task-management-api.git
+   cd task-manager
+
+2. Установите зависимости:
+    pip install -r requirements.txt
+
+3. Запустите приложение:
+    python app.py
+
+# Использование API
+
+GET /api/tasks — получить список всех задач.
+    curl http://127.0.0.1:5000/api/tasks
+
+POST /api/tasks — добавить новую задачу.
+    curl -X POST -H "Content-Type: application/json" -d '{"title":"Новая задача"}' http://127.0.0.1:5000/api/tasks
+
+# Просмотр задач
+Откройте в браузере: http://127.0.0.1:5000/tasks
+
+# Планы на будущее
+## Добавить базу данных SQLite для постоянного хранения задач.
+
+## Реализовать обновление и удаление задач.
+
+## Добавить авторизацию.
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import asyncio
 import uvicorn
 
 from asgiref.wsgi import WsgiToAsgi
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -11,16 +10,30 @@ api = Api(app)
 
 tasks_list = []
 
+class TaskResource(Resource):
+    def get(self):
+        return jsonify(tasks_list)
+
+    def post(self):
+        data = request.get_json()
+        if not data or 'title' not in data:
+            return {'error' : 'Title is required'}, 400
+        new_task = {
+            'id' : len(tasks_list) +1,
+            "title" : data['title'],
+            'status' : 'todo'
+        }
+        tasks_list.append(new_task)
+        return new_task, 201
+
+api.add_resource(TaskResource, '/api/tasks')
+
 @app.route('/')
 def index():
-    return "Task Management API"
+    return "Task Management API - Use /tasks to see tasks or /api/tasks for REST API"
 
-@app.route('/tasks', methods=['GET', 'POST'])
+@app.route('/tasks', methods=['GET'])
 def tasks():
-    if request.method == 'POST':
-        data = request.form
-        new_task = {'id' : len(tasks_list) + 1, 'title' : data['title'], 'status' : 'todo'}
-        tasks_list.append(new_task)
     return render_template('tasks.html', tasks=tasks_list)
 
 if __name__ == '__main__':
