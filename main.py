02929c18a88230601177c73288949efd6ec1609c
Kreesk <dwal67@gmail.com>
2025-03-14 19:41:45 +0300

    Add SQLite database support for task storage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+import sqlite3
 
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask, render_template, request, jsonify
@@ -8,23 +9,34 @@ app = Flask(__name__)
 asgi_app = WsgiToAsgi(app)
 api = Api(app)
 
-tasks_list = []
+def init_db():
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS tasks 
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, status TEXT)''')
+    conn.commit()
+    conn.close()
 
 class TaskResource(Resource):
     def get(self):
-        return jsonify(tasks_list)
+        conn = sqlite3.connect('tasks.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM tasks")
+        tasks = [{'id' : row[0], 'title' : row[1], 'status' : row[2]} for row in c.fetchall()]
+        conn.close()
+        return jsonify(tasks)
 
     def post(self):
         data = request.get_json()
         if not data or 'title' not in data:
             return {'error' : 'Title is required'}, 400
-        new_task = {
-            'id' : len(tasks_list) +1,
-            "title" : data['title'],
-            'status' : 'todo'
-        }
-        tasks_list.append(new_task)
-        return new_task, 201
+        conn = sqlite3.connect('tasks.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO tasks (title, status) VALUES (?, ?)", (data['title'], 'todo'))
+        conn.commit()
+        task_id = c.lastrowid
+        conn.close()
+        return {'id' : task_id, 'title' : data['title'], 'status' : 'todo'}, 201
 
 api.add_resource(TaskResource, '/api/tasks')
 
@@ -34,7 +46,13 @@ def index():
 
 @app.route('/tasks', methods=['GET'])
 def tasks():
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM tasks')
+    tasks_list = [{'id' : row[0], 'title' : row[1], 'status' : row[2]} for row in c.fetchall()]
+    conn.close()
     return render_template('tasks.html', tasks=tasks_list)
 
 if __name__ == '__main__':
+    init_db()
     uvicorn.run(asgi_app, host='127.0.0.1', port=5000)
